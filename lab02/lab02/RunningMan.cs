83fb6b03b2c42f7d67dd078c29aa5d4195469c6b
LawCheukHung <80800524+LawCheukHung@users.noreply.github.com>
2021-09-21 11:21:29 +0800

    updates

---
+++ lab02/lab02/RunningMan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace lab02
+{
+    class RunningMan : DrawableGameComponent
+    {
+        private Vector2 position, velocity;
+        private Texture2D texture;
+        private int frameCount, currentFrame;
+        private double frameElapsedTime, frameTimeStep;
+        private Rectangle frameRect;
+        private SpriteBatch _spriteBatch;
+
+        public RunningMan(Game g) : base(g) { }
+    }
+}
