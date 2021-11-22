ae10a7ba13267e2e66371f8ed613a580f2015c6e
LawCheukHung <80800524+LawCheukHung@users.noreply.github.com>
2021-11-22 13:07:44 +0800

    updates

---
+++ lab7_8_9/3DGame/3DGame/Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _3DGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Model groundModel;
+        Matrix view, project;
+
+        Windmill mill;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            view = Matrix.CreateLookAt((new Vector3(0, 0.9f, 0)), new Vector3(0, 0.9f, -0.5f), Vector3.Up);
+            project = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000);
+            mill = new Windmill(this, new Vector3(0, 0.9f, -4));
+            mill.view = view;
+            mill.project = project;
+            Components.Add(mill);
+
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            groundModel = Content.Load<Model>("ground");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            groundModel.Draw(Matrix.CreateScale(0.001f), view, project);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ lab7_8_9/3DGame/3DGame/Windmill.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DGame
+{
+    class Windmill : DrawableGameComponent
+    {
+        Model baseModel, fanModel;
+        Vector3 position;
+        float rotateAngle, rotateSpeed = 0.02f;
+        internal Matrix view, project;
+
+        public Windmill(Game game, Vector3 pos) : base(game)
+        {
+            position = pos;
+        }
+
+        protected override void LoadContent()
+        {
+            baseModel = Game.Content.Load<Model>("base");
+            fanModel = Game.Content.Load<Model>("fan");
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            rotateAngle = (rotateAngle + rotateSpeed) % MathHelper.TwoPi;
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Matrix world;
+            world = Matrix.CreateScale(10) * Matrix.CreateTranslation(position);
+            baseModel.Draw(world, view, project);
+            world = Matrix.CreateScale(10) * Matrix.CreateRotationZ(rotateAngle) * Matrix.CreateTranslation(position);
+            fanModel.Draw(world, view, project);
+
+            base.Draw(gameTime);
+        }
+    }
+}
